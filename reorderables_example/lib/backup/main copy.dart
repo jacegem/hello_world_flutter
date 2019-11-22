3d0a60fedf99de3765992a9c4207ae02c176b646
John <john@buzzni.com>
2019-11-22 12:01:33 +0900

    Add reorderable example

---
+++ reorderables_example/lib/backup/main copy.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:reorderables_example/wrap_example.dart';
+
+// import './table_example.dart';
+// import './nested_wrap_example.dart';
+// import './column_example1.dart';
+// import './column_example2.dart';
+// import './row_example.dart';
+// import './sliver_example.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Reorderables Demo',
+      home: MyHomePage(title: 'Reorderables Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+  final List<Widget> _examples = [
+    // TableExample(),
+    WrapExample(),
+    // NestedWrapExample(),
+    // ColumnExample1(),
+    // ColumnExample2(),
+    // RowExample(),
+    // SliverExample(),
+    WrapExample(),
+    WrapExample(),
+    WrapExample(),
+    WrapExample(),
+    WrapExample(),
+    WrapExample(),
+  ];
+  final _bottomNavigationColor = Colors.blue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: _examples[_currentIndex],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex:
+            _currentIndex, // this will be set when a new tab is tapped
+//        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.grid_on, color: _bottomNavigationColor),
+            title: Text('ReroderableTable',
+                maxLines: 2, style: TextStyle(color: _bottomNavigationColor)),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.apps, color: _bottomNavigationColor),
+            title: Text('ReroderableWrap',
+                maxLines: 2, style: TextStyle(color: _bottomNavigationColor)),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.view_quilt, color: _bottomNavigationColor),
+            title: Text('Nested ReroderableWrap',
+                maxLines: 3, style: TextStyle(color: _bottomNavigationColor)),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.more_vert, color: _bottomNavigationColor),
+              title: Text('ReroderableColumn 1',
+                  maxLines: 2,
+                  style: TextStyle(color: _bottomNavigationColor))),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.more_vert, color: _bottomNavigationColor),
+              title: Text('ReroderableColumn 2',
+                  maxLines: 2,
+                  style: TextStyle(color: _bottomNavigationColor))),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.more_horiz, color: _bottomNavigationColor),
+              title: Text('ReroderableRow',
+                  maxLines: 2,
+                  style: TextStyle(color: _bottomNavigationColor))),
+          BottomNavigationBarItem(
+              icon:
+                  Icon(Icons.calendar_view_day, color: _bottomNavigationColor),
+              title: Text('ReroderableSliverList',
+                  maxLines: 2,
+                  style: TextStyle(color: _bottomNavigationColor))),
+        ],
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
---
+++ reorderables_example/lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:bubble_bottom_bar/bubble_bottom_bar.dart';
+import 'package:flutter/material.dart';
+
+// import './table_example.dart';
+import './wrap_example.dart';
+// import './nested_wrap_example.dart';
+// import './column_example1.dart';
+// import './column_example2.dart';
+// import './row_example.dart';
+// import './sliver_example.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Reorderables Demo',
+      home: MyHomePage(title: 'Reorderables Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+  final List<Widget> _examples = [
+    // TableExample(),
+    WrapExample(),
+    // NestedWrapExample(),
+    // ColumnExample1(),
+    // ColumnExample2(),
+    // RowExample(),
+    // SliverExample(),
+    WrapExample(),
+    WrapExample(),
+    WrapExample(),
+    WrapExample(),
+    WrapExample(),
+    WrapExample(),
+  ];
+  final _bottomNavigationColor = Colors.blue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: _examples[_currentIndex],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      bottomNavigationBar: BubbleBottomBar(
+        opacity: .2,
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+        elevation: 8,
+        fabLocation: BubbleBottomBarFabLocation.end,
+        hasInk: true,
+        hasNotch: true,
+        inkColor: Colors.black12, //optional, uses theme color if not specified
+        items: <BubbleBottomBarItem>[
+          BubbleBottomBarItem(
+              backgroundColor: Colors.red,
+              icon: Icon(
+                Icons.dashboard,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.dashboard,
+                color: Colors.red,
+              ),
+              title: Text("Home")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.deepPurple,
+              icon: Icon(
+                Icons.access_time,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.access_time,
+                color: Colors.deepPurple,
+              ),
+              title: Text("Logs")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.indigo,
+              icon: Icon(
+                Icons.folder_open,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.folder_open,
+                color: Colors.indigo,
+              ),
+              title: Text("Folders")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.green,
+              icon: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.menu,
+                color: Colors.green,
+              ),
+              title: Text("Menu"))
+        ],
+      ),
+    );
+  }
+}
